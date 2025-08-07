f468c7a9dbe8b88e03e686f57230283b95908114
venunisandhan <venunisandhan@gmail.com>
2025-08-07 10:41:28 +0530

    route structures added

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello students" });
 });
 
+// CRUD functionality of movies
+
+// R - For Reading
+
+app.get("/movies", () => {});
+
+// C - For creating movies
+
+app.post("/movies", () => {});
+
+// U - For Updating Movies
+
+app.put("/movies/:id", () => {});
+
+//D - For delete movie
+
+app.delete("/movies/:id", () => {});
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 });
