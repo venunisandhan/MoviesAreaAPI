740cc095bf48be45f4368547d4331f0bcf44e9e0
venunisandhan <venunisandhan@gmail.com>
2025-08-07 10:30:59 +0530

    feat : App Structure

---
+++ main.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello students" });
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running at http://localhost:${PORT}`);
+});
